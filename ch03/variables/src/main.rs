16269616ca7742ea5f3d2e97d8988bdebe6e483a
Andrew Southwick <andrew@southwick.net>
2023-03-26 09:17:45 -0700

    The Rust Book 2023 Training
    Committing stable baseline relative to work completed in Chapter 3.  Review of memory safety aspects with an array and attempting to access a value outside of the bounds of the array.  Rust panics and exits the program with a runtime exception, similar to Java.  Apparently in C or C++ this type of check is manual, and can lead to major security and or run time bugs.  Committing baseline with all examples visible for quick reference.

--- ch03/variables/src/main.rs
+++ ch03/variables/src/main.rs
@@ -1,4 +1,37 @@
+use std::io;
+
 fn main() {
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line.");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+
+    /*
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = x.0;
+    let _six_point_four = x.1;
+    let _one = x.2;
+
+    let _a = [1, 2, 3, 4, 5];
+     */
+
+    /*
     let x = 5;
     let x = x + 1;
     {
@@ -7,6 +40,7 @@ fn main() {
     }
 
     println!("The value ox x is: {x}");
+    */
 
     /*
     let mut x = 5;
